5c2a77b71419b3dc44c19bbbc29c87c396288f7d
SamuelMaged <eng.samuel.maged@gmail.com>
2019-09-15 16:28:04 +0200

    f

---
+++ Course 1  Java Programming Solving Problems with Software/Week 2/StringsFirstAssignments/Part2.java
@@ -0,0 +1,50 @@
+
+/**
+ * Write a description of Part2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part2 {
+public String findSimpleGene(String dna)
+{
+    int startIndex = dna.indexOf("ATG");
+    if(startIndex==-1){
+        return " No gene was found ";
+    }
+    int endIndex = dna.indexOf("TTA", startIndex+3);
+    if(endIndex == -1)
+    {
+        return " No gene was found ";
+    }
+    if( (startIndex-endIndex)%3 != 0)
+    {
+        return " No gene was found " ;
+    }
+    
+    return dna.substring(startIndex,endIndex+3);
+}
+public void testSimpleGene()
+{
+    String dna = "GTEATGEETGTEETGTTAER";
+    System.out.println("The dna is "+ dna);
+    String gene = findSimpleGene(dna);
+    System.out.println(gene);
+    dna = "GTEATGEETGTEETGTTER";
+    System.out.println("The dna is "+ dna);
+    gene = findSimpleGene(dna);
+    System.out.println(gene);
+    dna = "GTEATGEETGTETGTTAER";
+    System.out.println("The dna is "+ dna);
+    gene = findSimpleGene(dna);
+    System.out.println(gene);
+    dna = "GTEATEETGTEETGTTAER";
+    System.out.println("The dna is "+ dna);
+    gene = findSimpleGene(dna);
+    System.out.println(gene);
+    dna = "GTEEETGTEETGER";
+    System.out.println("The dna is "+ dna);
+    gene = findSimpleGene(dna);
+    System.out.println(gene);
+}
+}
