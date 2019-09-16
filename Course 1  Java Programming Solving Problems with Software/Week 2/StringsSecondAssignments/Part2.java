1fc091898edbb57364ef83f3cb20ef62fed4857c
SamuelMaged <eng.samuel.maged@gmail.com>
2019-09-16 14:20:35 +0200

    Part2

--- Course 1  Java Programming Solving Problems with Software/Week 2/StringsSecondAssignments/Part2.java
+++ Course 1  Java Programming Solving Problems with Software/Week 2/StringsSecondAssignments/Part2.java
@@ -6,5 +6,13 @@
  * @version (a version number or a date)
  */
 public class Part2 {
-
+    public int howMany(String stringa,String stringb){
+        int occur = 0;
+        int currIndex = stringb.indexOf(stringa);
+        while(currIndex != -1){
+            occur++;
+            currIndex = stringb.indexOf(stringa,currIndex + stringa.length());
+        }
+        return occur;
+    }
 }
