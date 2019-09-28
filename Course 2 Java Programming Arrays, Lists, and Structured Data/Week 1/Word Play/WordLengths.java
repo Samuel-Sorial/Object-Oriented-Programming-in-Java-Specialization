a4f0f0d4418cf2d5a06cbcbe619b896a46cf69fa
SamuelMaged <eng.samuel.maged@gmail.com>
2019-09-28 20:34:43 +0200

    Breaking the Caesar Chiper part1

---
+++ Course 2 Java Programming Arrays, Lists, and Structured Data/Week 1/Word Play/WordLengths.java
@@ -0,0 +1,47 @@
+import edu.duke.*;
+
+/**
+ * Write a description of WordLengths here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WordLengths {
+    public int[] countWordLengths(FileResource resource, int[] counts)
+    {
+        for(String word : resource.words())
+        {
+        int length = word.length();
+         if(Character.isLetter(word.charAt(0)) && Character.isLetter(word.charAt(length-1)))
+           {
+             if(length <= counts.length)
+             {
+                counts[length]+=1;
+             }
+             else{
+                 counts[counts.length-1]+=1;
+             }
+        }
+        else{
+             if(length <= counts.length)
+             {
+                counts[length-1]+=1;
+             }
+             else{
+                 counts[counts.length-1]+=1;
+             }
+        }
+    }
+    return counts;
+   }
+   public void testCountWordLengths()
+   {
+       FileResource fr = new FileResource();
+       int[] counts = new int[31];
+       counts = countWordLengths(fr,counts);
+       for(int i=0; i<counts.length; i++)
+       {
+           System.out.println(i + " appeard " + counts[i]);
+        }
+       }
+}
